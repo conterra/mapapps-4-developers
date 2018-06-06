328d6950d968d9208aefdbf67c66f8e5432cd322
Julian Kuhlmann <j.kuhlmann@conterra.de>
2018-06-06 14:16:30 +0200

    setting upp themes compiling mechanism

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,20 @@ const run_sequence = require('run-sequence');
 const mapapps = require('ct-mapapps-gulp-js');
 
 mapapps.registerTasks({
-    themes: [/*put custom themes here*/],
+    /* a list of themes inside this project */
+    themes: ['theme-custom'],
+    /* state that the custom theme will be dependant from map.apps everlasting theme that provides the base styles */
     hasBaseThemes: true,
-    forceTranspile: true
+    /* state that we want to support vuetify components and therefore need the the vuetify core styles*/
+    hasVuetify: true,
+    forceTranspile: true,
+    themesSrcLocation: "./src/main/js/bundles",
+    themesDestLocation: "./target/webapp/js/bundles",
+    themeChangeTargets: {
+       "vuetify": [
+           "theme-custom"
+       ]
+   }
 });
 
 gulp.task("default", function(callback) {
