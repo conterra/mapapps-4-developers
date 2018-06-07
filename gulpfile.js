5f3903a4094a91951dd2e4702090f8cff93a541c
Julian Kuhlmann <j.kuhlmann@conterra.de>
2018-06-07 11:54:48 +0200

    simplify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,13 @@ const run_sequence = require('run-sequence');
 const mapapps = require('ct-mapapps-gulp-js');
 
 mapapps.registerTasks({
+    /* A detailed description of available setting is available at https://www.npmjs.com/package/ct-mapapps-gulp-js */
     /* a list of themes inside this project */
     themes: ['theme-custom'],
     /* state that the custom theme will be dependant from map.apps everlasting theme that provides the base styles */
     hasBaseThemes: true,
     /* state that we want to support vuetify components and therefore need the the vuetify core styles*/
     hasVuetify: true,
-    forceTranspile: true,
-    themesSrcLocation: "./src/main/js/bundles",
-    themesDestLocation: "./target/webapp/js/bundles",
     themeChangeTargets: {
        "vuetify": [
            "theme-custom"
