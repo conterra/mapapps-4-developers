d1b8f43cd26ab7b530816bd6bbf186fd1bf202cd
Matthias Stein <m.stein@conterra.de>
2020-10-29 13:21:17 +0100

    Comment a part of the packages configuration in the test-init.js out to support tests in include-mapapps-deps profile

--- src/test/webapp/js/tests/test-init.js
+++ src/test/webapp/js/tests/test-init.js
@@ -7,16 +7,16 @@ testConfig({
             ]
         }
         //uncomment, if project runs in remote mode
-        ,{
-            root: "@@mapapps.remote.base@@/resources/jsregistry/root",
-            packages: [
-                "apprt-polyfill",
-                "apprt@4.x",
-                "apprt-vue@4.x",
-                "vuetify@~0.14.7",
-                "esri@4.x"
-            ]
-        }
+        // ,{
+        //     root: "@@mapapps.remote.base@@/resources/jsregistry/root",
+        //     packages: [
+        //         "apprt-polyfill",
+        //         "apprt@4.x",
+        //         "apprt-vue@4.x",
+        //         "vuetify@~0.14.7",
+        //         "esri@4.x"
+        //     ]
+        // }
     ],
     // ensure babel polyfill is loaded during test execution
     deps: [
