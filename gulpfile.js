1be33840d2dabd997efd150c67db161d2fa4976f
mre <m.reiprecht@conterra.de>
2021-08-30 15:39:24 +0200

    correct scope of test packate

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ mapappsBrowserSync.registerTask({
         npmModules: [
             "mocha",
             "chai",
-            "@conterra-dev/mapapps-mocha-runner"
+            "@conterra/mapapps-mocha-runner"
         ]
     }
 }, gulp);
