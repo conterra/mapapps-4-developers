4f190dfec170d86f3a3bf156caf9c14c8e91591a
Julian Kuhlmann <j.kuhlmann@conterra.de>
2022-02-01 10:52:51 +0100

    changes required for singular theme extension

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,12 @@ const mapapps = require('ct-mapapps-gulp-js');
 
 mapapps.registerTasks({
     /* A detailed description of available setting is available at https://www.npmjs.com/package/ct-mapapps-gulp-js */
+
     /* a list of themes inside this project */
-    themes: ['theme-custom'],
-    /* state that the custom theme will be dependant from map.apps everlasting theme that provides the base styles */
-    hasBaseThemes: true,
-    /* state that we want to support vuetify components and therefore need the the vuetify core styles*/
-    hasVuetify: true,
-    themeChangeTargets: {
-        "vuetify": [
-            "theme-custom"
-        ]
-    }
+    themes: ['everlasting'],
+
+    themesSrcLocation: "./src/main/js/bundles/custom-theme-extension",
+    themesDestLocation: "./target/webapp/js/bundles/custom-theme-extension"
 });
 
 gulp.task("default",
