d958dfb674a37d9010b3f3f16e67284b0fde3723
Michael Beckemeyer <m.beckemeyer@conterra.de>
2022-06-28 11:12:16 +0200

    Add project license and include a script to check licenses of dependencies

---
+++ src/support/js/check-licenses.js
@@ -0,0 +1,57 @@
+/*
+    Checks licenses of node dependencies against a list of allowed licenses.
+    Fails with exit code != 0 and an error message if a disallowed license is encountered.
+    The script should be executed from the project root directory after dependencies have been installed:
+
+        $ ./node/node ./src/support/js/check-licenses.js
+
+    To run checks yourself (e.g. to update the allow list or to get details), install
+    license-checker yourself and run it from the project root directory:
+
+        $ npm install -g license-checker
+        $ license-checker --summary # outputs list of used licenses
+        $ license-checker --json    # outputs details
+
+    See also https://www.npmjs.com/package/license-checker
+*/
+
+const checker = require("license-checker");
+const process = require("process");
+
+// Licenses known to be OK.
+const ACCEPTED_LICENSES = [
+    "MIT",
+    "ISC",
+    "Apache-2.0",
+    "BSD-2-Clause",
+    "BSD-3-Clause",
+    "BSD",
+    "CC0-1.0",
+    "CC-BY-3.0",
+    "CC-BY-4.0",
+    "Python-2.0",
+    "Unlicense" // Note: not unlicenseD (https://opensource.org/licenses/unlicense)
+];
+
+// Packages which licenses are not recognized properly by license-checker.
+// These must be checked manually.
+const SKIP_PACKAGES = [
+    "event-stream@3.0.20", // MIT License not recognized
+    "taffydb@2.6.2", // BSD-1-Clause License in source code
+    "mapapps-4-developers@0.0.1" // TODO remove once license has been added
+];
+
+checker.init(
+    {
+        start: process.cwd(),
+        onlyAllow: ACCEPTED_LICENSES.join(";"),
+        excludePackages: SKIP_PACKAGES.join(";")
+    },
+    (error, packages) => {
+        if (error) {
+            console.error("Error: ", error);
+            process.exit(1);
+        }
+        process.exit(0);
+    }
+);
