06245d447d3a28b87fb8e70d8f0bc432c38bbb4c
Andre Frank <a.frank@conterra.de>
2023-11-29 12:34:16 +0100

    suppress warning: eslint-disable-next-line no-undef for testConfig

--- src/test/webapp/js/tests/test-init.js
+++ src/test/webapp/js/tests/test-init.js
@@ -1,11 +1,11 @@
+// eslint-disable-next-line no-undef
 testConfig({
     jsregistry: [{
-            //root: "url to registry..",
-            packages: [
-                // register all self hosted packages
-                "*"
-            ]
-        }
+        //root: "url to registry..",
+        packages: [
+            // register all self hosted packages
+            "*"
+        ]}
         //uncomment, if project runs in remote mode
         // ,{
         //     root: "@@mapapps.remote.base@@/resources/jsregistry/root",
