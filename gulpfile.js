4a582f079447a6ddb7641547ab00da55a95992ce
Marko Reiprecht <m.reiprecht@conterra.de>
2025-10-21 14:17:47 +0200

    MAPAPPS-7526 - add dotenv support

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require("gulp");
 const mapapps = require('ct-mapapps-gulp-js');
 const mapappsBrowserSync = require("ct-mapapps-browser-sync");
+const dotEnv = require("dotenv");
+// load .env file if it exits
+// the local dev jsregistry will lookup any @@key.property@@ expression, also as environment variable with name "KEY_PROPERTY" for replacement.
+dotEnv.config();
 
 const isProduction = process.env.NODE_ENV === "production";
 console.info(`Configuring gulp build for ${isProduction ? "production" : "development"}`);
